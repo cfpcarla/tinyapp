242fe83d4ec3a1c3d4b7d8829af000f9e3eeb019
Carla <cfpcarla@gmail.com>
2019-12-10 20:37:06 +0000

    implement delete

--- express_server.js
+++ express_server.js
@@ -72,3 +72,10 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+
+  res.redirect('/urls');
+});
